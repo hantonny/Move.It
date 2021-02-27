1757dc176894f62d49fa6eb9594344a50de32de4
Hantonny Korrea <hantonny.20@gmail.com>
2021-02-27 15:20:05 -0400

    :sparkles: - Contexto e componentes

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://pbs.twimg.com/profile_images/1360958432363376661/rjcUyf0a_400x400.jpg" alt="Hantonny Korrea"/>
@@ -11,7 +14,7 @@ export function Profile() {
                 </strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
