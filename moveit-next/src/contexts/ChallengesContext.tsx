e040c41ad6d92b3ba3ebfd743c312a6661b37dc9
Hantonny Korrea <hantonny.20@gmail.com>
2021-02-28 12:27:51 -0400

    :sparkles: - Melhorando a usabilidade da aplicação

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 
 import challenges from '../../challenges.json';
 
@@ -17,6 +17,7 @@ interface ChallengesContextData {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
 
 }
 
@@ -29,12 +30,16 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 export function ChallengesProvider({children}: ChallengesProviderProps) {
 
     const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(35);
+    const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
     const [activeChallenge, setActiveChallenge] = useState(null);
 
-    const experienceToNextLevel  = Math.pow((level + 1) * 4, 2)
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+    
+    useEffect(() => {
+        Notification.requestPermission()
+    }, [])
 
     function levelUp() {
       setLevel(level + 1);
@@ -45,12 +50,40 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge)
+
+        new Audio('/notification.mp3').play();
+
+        if (Notification.permission === 'granted') {
+            new Notification('Novo desafio 🎉️', {
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge() {
         setActiveChallenge(null);
     }
 
+    function completeChallenge() {
+
+        if (!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge;
+        
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience  = finalExperience - experienceToNextLevel;
+            levelUp();
+            
+        }
+
+        setCurrentExperience(finalExperience)
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1)
+    }
 
     return (
         <ChallengesContext.Provider
@@ -62,7 +95,8 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
                 startNewChallenge,
                 activeChallenge,
                 resetChallenge,
-                experienceToNextLevel
+                experienceToNextLevel,
+                completeChallenge
             }}
         >
             {children}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -7,6 +7,7 @@ import { Profile } from "../components/Profile";
 
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
         <title>Inicio | move.it</title>
       </Head>
       <ExperienceBar />
-      
+      <CountdownProvider>
       <section>
         <div>
           <Profile />
@@ -25,7 +26,8 @@ export default function Home() {
         <div>
           <ChallengeBox/>
         </div>
-      </section>
+        </section>
+        </CountdownProvider>
   </div>
   )
 }
