0261d1c718568480eb6fd9660c6cc2664fd2234b
Hantonny Korrea <hantonny.korrea@nastek.com.br>
2024-02-06 16:57:46 -0400

    Tirar nome

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,17 @@
-import { useContext } from 'react';
-import { ChallengesContext } from '../contexts/ChallengesContext';
-import styles from '../styles/components/Profile.module.css';
-
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
-    const { level } = useContext(ChallengesContext);
-    return (
-        <div className={styles.profileContainer}>
-            {/* <img src="https://pbs.twimg.com/profile_images/1360958432363376661/rjcUyf0a_400x400.jpg" alt="Hantonny Korrea"/> */}
-            <div>
-                <strong>
-                    Hantonny Korrea
-                </strong>
-                <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level {level}
-                </p>
-            </div>
-        </div>
-    )
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <div>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
 }
